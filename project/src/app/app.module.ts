b2631642b26da961d0f7f027b23df892f0ff2861
mohammedtlemsani <mohammed.tlemsani1-etu@etu.univh2c.ma>
2024-04-10 01:11:16 +0000

    product.model added

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
 import { NewProductComponent } from './new-product/new-product.component';
+import {HttpClientModule} from "@angular/common/http";
+import {ProductService} from "./services/product.service";
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { NewProductComponent } from './new-product/new-product.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- project/src/app/products/products.component.ts
+++ project/src/app/products/products.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {ProductService} from "../services/product.service";
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit{
+  constructor(private ps:ProductService) {
+  }
+  products :Array<any>=[];
 
+  ngOnInit(): void {
+    this.ps.getProducts().subscribe({
+
+        next : data => this.products=data,
+        error: err => console.log(err)
+      })
+  }
+
+  handleCheckProduct(product: any) {
+    console.log(product.id);
+
+    this.ps.checkProduct(product).subscribe(
+        (response) => {
+          console.log('Product updated successfully:', response);
+          product.checked = !product.checked;
+        },
+        (error) => {
+          console.error('Error updating product:', error);
+        }
+      );
+  }
 }
